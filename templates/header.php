22bafe40d1e482e047ca309f51fef4e26300c688
Antiiee <antar420@student.liu.se>
2014-03-04 22:58:13 +0100

    bit of refactoring

---
+++ templates/header.php
@@ -0,0 +1,20 @@
+         <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Project</a>
+                    </div>
+
+                    <div class="collapse navbar-collapse" id="navbar-collapse-target">
+                     <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php" id="Parallell" style="cursor:pointer;">Parallell Coordinates</a></li>
+                        <li><a href="scatter.php" id="Scatter" style="cursor:pointer;">Scatter plot</a></li>
+                     </ul>
+                   </div>
+                </div>
+            </div>
