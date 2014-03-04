2009fc3641c7437023f15e0bb2ba03a2f6550371
kaajtan <tobjo423@student.liu.se>
2014-03-04 09:54:33 +0100

    modifierat pca och lagt till lite

--- js/pca.js
+++ js/pca.js
@@ -4,7 +4,7 @@ function pca(){
     console.log("hehe");
 
     //Load data
-    d3.csv("data/nytest.csv", function(data) {
+    d3.csv("data/test.csv", function(data) {
 
         self.data = data;
 
@@ -14,33 +14,100 @@ function pca(){
     function covariance(value){
 
         var dimensions = 0;
-        var ev;
+        var evMatrix;
+        var centered;
+        var centeredMatrix;
+        var dataPoints;
+        var sum = 0;
+        var covarianceMatrix;
+        var plott = new Array();
         for(var val in value[0]){
             dimensions++;
         }
 
         var avg = avgValue(value);
-        var cm = covarianceMatrix(value, avg, dimensions);
+
+        //covariance matrix and centered matrix
+        var cacdm = covAndCDMatrix(value, avg, dimensions);
+        centered = cacdm.bar;
+        covarianceMatrix = cacdm.cov;
+
+        //eigenvectors and eigenvalues from covariance matrix
+        var eig = numeric.eig(covarianceMatrix);
+
+        centeredMatrix = Matrix.create(centered);
+        evMatrix = Matrix.create(eig.E.x);
+
+        for(var i = 0; i<dimensions; i++){
+            plott[i] = new Array();
+        }
+
+        //datavalues to be plotted
+        dataPoints = centeredMatrix.multiply(evMatrix);
+        
+        var lambda = eig.lambda.x;
+        for(var i = 0; i<lambda.length; i++){
+            sum = parseFloat(lambda[i]) + sum;
+        }   
+
+        var dataScore = new Array();
+        for(var i = 0; i<lambda.length; i++){
+            dataScore[i] = lambda[i]/sum;
+        }
+
+        /*for(var i = 0; i<dimensions; i++){
+            for(var j = 0; j<dataPoints.elements.length;j++){
+                sum = parseFloat(dataPoints.elements[j][i]) + sum;
+            }
+            console.log("sadas: " + dataPoints.elements[9][1]);
+            console.log("----------------");
+            sum = sum/(dataPoints.elements.length-1);
+            plott[i] = sum;
+            sum=0;
+        }   */      
+
+
+        //centered = Matrix.create(cacdm.bar);
+        //covarianceMatrix = Matrix.create(cacdm.cov);
 
     };
 
-    function covarianceMatrix(value, avg, dim){
+    //covariance and centered data matrix
+    function covAndCDMatrix(value, avg, dim){
 
-        var faktor1, faktor2, coValue, dim1, dim2, sum = 0;
+        var faktor1 = 0;
+        var faktor2 = 0;
+        var coValue = 0;
+        var sum = 0;
+        var dim1 = 0;
+        var dim2 = 0;
         var covMatrix = new Array();
-        
+        var dimBar = new Array();
+        var obj = {
+            "bar": "",
+            "cov": ""
+        };
         for(var i = 0; i<dim; i++){
             covMatrix[i] = new Array();
+        }
+        for(var i = 0; i<value.length; i++){
+            dimBar[i] = new Array();
         }
 
         for(var val in value[0]){
             for(var val2 in value[0]){
                 for(var i = 0; i<value.length;i++){
-                    faktor1 = parseInt(value[i][val]) - avg[val];
-                    faktor2 = parseInt(value[i][val2]) - avg[val2];
+                    if(val == val2){
+                        dimBar[i][dim2] = parseFloat(value[i][val]) - avg[val2];
+                    }
+
+                    faktor1 = parseFloat(value[i][val]) - avg[val];
+                    faktor2 = parseFloat(value[i][val2]) - avg[val2];
+                    
                     sum += (faktor1*faktor2);
                     faktor1 = 0;
                     faktor2 = 0;
+
                 }
                 sum = sum / (value.length-1);
                 covMatrix[dim1][dim2] = sum;
@@ -51,7 +118,9 @@ function pca(){
             dim2 = 0;
         }
 
-        return covMatrix;
+        obj.bar = dimBar;
+        obj.cov = covMatrix;
+        return obj;
     };
 
     function avgValue(value){
@@ -62,7 +131,7 @@ function pca(){
         for(var j in value[0]){
             for(var i = 0; i<size;i++){
 
-                first = parseInt(value[i][j]) + first;
+                first = parseFloat(value[i][j]) + first;
 
             }
 
@@ -70,7 +139,6 @@ function pca(){
             first = 0; 
             
         }
-        console.log(avg);
         return avg;
     };
 
